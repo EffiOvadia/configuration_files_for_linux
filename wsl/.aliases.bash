5ef16734004c40dafad6f6e2e44c25bcd7e6172c
Effi Ovadia <Effi.Ovadia@GMail.com>
2023-05-25 03:20:07 +0800

    modified:   wsl/.aliases.bash

--- wsl/.aliases.bash
+++ wsl/.aliases.bash
@@ -1,3 +1,8 @@
 
+#! Fix the aliases 
 \cp -Rfv /mnt/c/cygwin64/git/profile_environment_for_linux/user/. ~/
 \cp -Rfv /mnt/c/cygwin64/git/profile_environment_for_linux/user/. /home/effio/
+
+cd ~ && . .effio/.runonce.bash && su - effio
+
+. .effio/.runonce.bash
