bc619930ecf54b6b8eb40af50feb8c92b396bde1
Effi Ovadia <8449503+EffiOvadia@users.noreply.github.com>
2023-06-10 13:55:41 +0800

    deleted:    wsl/.aliases.bash

--- wsl/.aliases.bash
+++
@@ -1,8 +0,0 @@
-
-#! Fix the aliases 
-\cp -Rfv /mnt/c/cygwin64/git/profile_environment_for_linux/user/. ~/
-\cp -Rfv /mnt/c/cygwin64/git/profile_environment_for_linux/user/. /home/effio/
-
-cd ~ && . .effio/.runonce.bash && su - effio
-
-. .effio/.runonce.bash
